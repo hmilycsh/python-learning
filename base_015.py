549d0b673ce6b1e4ecd5440c2b0025c620651488
hmilycsh <pingzizzp@163.com>
2019-12-01 23:51:52 +0800

    end to base_20

---
+++ base_015.py
@@ -0,0 +1,11 @@
+# 题目：利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
+
+
+score = int(input("请输入分数："))
+if score >= 90:
+    grade = "A"
+elif score >= 60:
+    grade = "B"
+else:
+    grade = "C"
+print("分数%d的等级为%s" % (score, grade))
---
+++ base_016.py
@@ -0,0 +1,8 @@
+# 题目：输出指定格式的日期。
+
+
+import datetime
+
+
+if __name__ != 'main':
+    print(datetime.date.today().strftime('%Y-%m-%d'))
